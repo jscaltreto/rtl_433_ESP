cbe8504abc3c9b8664c6acf2aeb5a62cb3e6ab11
Jake Scaltreto <jake@scaltreto.com>
2025-01-19 16:45:37 -0500

    only include govee

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -279,13 +279,10 @@
  * Subset of devices that I have access to and have tested with
  */
 #  define DEVICES         \
-    DECL(acurite_986)     \
-    DECL(skylink_motion)  \
-    DECL(prologue)        \
-    DECL(philips_aj3650)  \
-    DECL(fineoffset_WH51) \
+    DECL(govee)           \
+    DECL(govee_h5054)     \
 /* Add new personal decoders here. */
-#  define NUMOFDEVICES 5
+#  define NUMOFDEVICES 2
 #endif
 
 #define DECL(name) extern r_device name;
